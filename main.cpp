776e41506c500a31763ef43b48fd86cd18f64d9d
Grey Hill <infogreytech@gmail.com>
2016-09-14 19:44:56 -0700

    Finish up to 2.2 on learncpp.com

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "main.h"
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	int x = 10; // This is copy initialization
+	int y(10); // direct initialization
+	float z{10}; // uniform initialization - preferred for c++11
 
-	std::cout << "This is a project for learning C++!" << std::endl;
+	// When a variable is given a value after it has been defined,
+	// it is called a copy assignment (or assignment for short)
+	int a;
+	a = 5;
+
+	std::cout << "x is " << x << std::endl;
 
 	return 0;
 }
