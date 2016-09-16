f69a47f60233b208747458e16da9bae1b4d4c41f
Grey Hill <infogreytech@gmail.com>
2016-09-15 19:43:26 -0700

    Finish chapter 2.7

--- main.cpp
+++ main.cpp
@@ -5,12 +5,44 @@ int main() {
 	int y(10); // direct initialization
 	float z{10}; // uniform initialization - preferred for c++11
 
-	// When a variable is given a value after it has been defined,
-	// it is called a copy assignment (or assignment for short)
-	int a;
-	a = 5;
-
 	std::cout << "x is " << x << std::endl;
+
+	int a; // This is a declaration
+	int b = 10; // This is initialization
+
+	// The sizeof operator returns the amount of bytes
+	// that a particular data type uses
+	// This is because C++ only guarantees a minimum 
+	// size for primitive types because of performance
+	// reasons on different architectures
+	std::cout << "int takes up " << sizeof(int) << " bytes" << std::endl;
+
+	float c = 5.0f; // Use f to indicate float
+	// Default is double
+
+	double d = 5e10; // = 5 x 10^10
+	// The e denotes scientific notation
+
+	// Because C++ is based on C, 
+	// 1 == true
+	// 0 == false
+	bool boolean = true;
+	std::cout << boolean << std::endl;
+	
+	// std::boolalpha makes it so std::cout prints
+	// out 'true' and 'false' instead of 1 and 0
+	std::cout << std::boolalpha;
+	std::cout << boolean << std::endl;
+
+
+	// Type casting
+	// use static_cast<new_type>(expression)
+	// to convert between different representations
+	int i{97};
+	char j{static_cast<char>(i)};
+
+	std::cout << i << " " << j << std::endl;
+
 
 	return 0;
 }
--- main.h
+++ main.h
@@ -1,1 +1,7 @@
 #include <iostream>
+// Used to output and input from command-line or standard output and input
+// eg:  std::cout << "Hello" << std::endl;
+
+#include <cstdint>
+// Defines fixed-width integers in the std namespace
+
