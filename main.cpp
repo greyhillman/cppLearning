afe1d31fbbe90466f89279a86d5d12990910a4cd
Grey Hill <infogreytech@gmail.com>
2016-09-29 19:14:50 -0700

    Update main.cpp
    main.cpp should only hold things that can be expressed in a block.
    
    Stuff like classes, global variables, structs, etc. should be in their own folder.

--- main.cpp
+++ main.cpp
@@ -77,205 +77,182 @@ int main() {
 
 	}
 
-	//int x = 5; //5 is a literal
-	// A literal is a constant that can't be changed.
-	// Sometimes literals have suffixes that are used to determine
-	// their type. But usually the compiler will figure it out.
+	printSeparator();
 
-	// A leading 0 on an integer means its a number in octal.
-	// 0x denotes a hexadecimal number.
-	// 0b denotes a binary number in C++14
-	// C++14 also allows using ' as a digit separator
-	int f = 0b0001'1011'1011;
-
-	// A magic number is a hard-coded literal that does not
-	// have any context.
-
-	// To make a variable constant, use the const keyword
-	const double g { 9.81 };
-	// Assigning to a const variable results in a compile-error
-	//g = 10; // will throw an compile error
-
-	// const variables can be initialized from non-const values.
-	int temp;
-	std::cout << "Enter a number: ";
-	std::cin >> temp;
-	std::cout << std::endl;
-	const int h { temp }; // run-time const
-
-	// C++ has two kinds of constants: compile-time and run-time
-	// const can be both compile-time and run-time
-	// constexpr is only used for compile-time
-	constexpr double compileConst = 123.0;
-	
-	// The best way to deal with constants is to have them all in one place.
-	// Like a header file in a namespace.
-
-
-	// Chapter 3.4: sizeof(), comma operator, and conditional operator
+	// different integer bases
 	{
-		// The sizeof() operator returns the number of bytes allocated for a
-		// variable or data type.
-		std::cout << "Number of bytes for int: " << sizeof(int) << std::endl;
-		std::cout << "Number of bytes for compileConst: " 
-				  << sizeof(compileConst) << std::endl;
+		// C++14 allow the use of ' as a digit separator
+		// C++14 allows use of binary numbers
+		int binary  = 0b0000'0000'1100;
+		int decimal = 12;
+		int octal   = 014;
+		int hex     = 0xC;
 
-		// The comma operator evualates all the expressions and then returns the right
-		// most expression
+		std::cout << "binary  = " << binary << std::endl;
+		std::cout << "decimal = " << decimal << std::endl;
+		std::cout << "octal   = " << octal << std::endl;
+		std::cout << "hex     = " << hex << std::endl;
+	}
+
+	printSeparator();
+
+	// literals
+	{
+		// Literals are constants that can't be changed
+		// like 5 below
+		int x = 5;
+
+		// Sometimes literals have suffixes that are used to
+		// determine their type.
+		// The compiler will usually figure it out.
+	}
+
+	printSeparator();
+
+	// const and constexpr
+	{
+		const double g {9.81}; // const keyword makes it constant
+
+		//g = 10; // This would result in error
+
+		// The compiler makes sure that a const is not modified
+
+		// const variables can be initialized from non-const values
+
+		int temp = 3;
+
+		const int x = temp; // compile- or run-time const
+
+		constexpr int y = 12; // compile-time const
+
+		// const can be both compile-time and run-time
+		// constexpr can only be for compile-time
+
+	}
+
+	printSeparator();
+
+	// TIP: The best way to deal with constants is to have them
+	// all in one place. Like a header file in a namespace
+
+	// comma operator
+	{
 		int x = 5;
 		int y = 6;
 		int z = (++x, ++y); // x = 6, y = 7, z = 7
-		
-		// Most programmers don't use this except in for-loops
 
-		// Conditional Operator
-		// <condition> ? <expression1> : <expression2>
-		// if condition is true, return expression1. Else return expression2.
+		// The comma operator evaluates all the expressions and then
+		// returns the right most expression
 
-		// Much similar to an if statement but the conditional operator evaluates
-		// as an expression. This means you can initialize a const based on a condition.
+		// Most programmers don't use this except in for loops
+	}
+
+	printSeparator();
+
+	// conditional operator
+	{
 		bool condition = true;
+
 		const int test = condition ? 30 : 20;
-		// You can't do this with an if-else statement.
+		// If condition is true, return 30. else return 20
+		std::cout << "test = " << test << std::endl;
 
-		// In general, use an if-else statement. But if the expression is very
-		// simple, and is more readable using a conditional operator, go ahead.
-
+		// TIP: Use if-else for most thing but if a conditional
+		// makes it more readable, use it.
 	}
 
-	// Chapter 3.5: Relational operators (comparisons)
+	printSeparator();
+
+	// logical operators
 	{
-		// Greater than: >
-		// Less than: <
-		// Greater than or equal: >=
-		// Less than or equal: <=
-		// Equality: ==
-		// Inequality: !=
-		// All return either true or false.
+		// they can short circuit
 
-		// You shouldn't use this operators when comparing floating-point numbers
-		// due to round-off errors.
-		// If you need to compare numbers that are close to each other,
-		// use a comparison function with some epsilon.
-
+		// != has the same effect as XOR
 	}
 
-	// Chapter 3.6: Logical operators
+	printSeparator();
+
+	// bitwise operators
 	{
-		// NOT: !<condition>
-		// AND: <condition> && <condition>
-		// OR:  <condition> || <condition>
+		unsigned short x {0b0000'1011};
+		unsigned short y {0b0000'0101};
 
-		// && and || have short circuit evaluation
+		std::cout << x << std::endl;
 
-		// There is not explicit XOR operator, but != has the same effect.
+		// << bit-shift left
+		std::cout << "x << 1 = " << (x << 1 ) << std::endl;
+		std::cout << "x << 2 = " << (x << 2 ) << std::endl;
+
+		// >> bit-shift right
+		std::cout << "x >> 1 = " << (x >> 1) << std::endl;
+		std::cout << "x >> 2 = " << (x >> 2) << std::endl;
+
+		// ~ bitwise not
+		std::cout << "~x = " << static_cast<unsigned short>(~x) << std::endl;
+		// We use static_cast<unsigned short> because std::cout was
+		// treating (~x) as a signed int
+
+		// & bitwise and
+		std::cout << "x & y = " << (x & y) << std::endl;
+
+		// | bitwise or
+		std::cout << "x | y = " << (x | y) << std::endl;
+
+		// ^ bitwise xor
+		std::cout << "x ^ y = " << (x ^ y) << std::endl;
+
+		// TIP: Only use bitwise operators on unsigned integers.
+		// Things might mess up with other data types
 	}
 
-	// Chapter 3.7: Converting between binary and decimal.
-	// Already know this stuff.
+	printSeparator();
 
-	// Chapter 3.8: Bitwise operators
+	// std::bitset
 	{
-		// Left shift : x << y : Moves bits in x left y times (times by 2)
-		// Right shift: x >> y : Moves bits in x right y times (divide by 2)
-		// bitwise NOT:  ~x    : Flip the bits
-		// bitwise AND: x & y  : Does AND on each pair of bits
-		// bitwise OR : x | y  : Does OR on each pair of bits
-		// bitwise XOR: x ^ y  : Does XOR on each pair of bits
-
-		// Because they work on all bits, a rule is to only use them on
-		// unsigned data types.
-
-		unsigned int x = 0b0001;
-		std::cout << "x << 1 = " << (x << 1) << std::endl;
-		// Should be 2.
-
-		x = 0b1000;
-		std::cout << "x >> 3 = " << (x >> 3) << std::endl;
-		// Should be 1
-
-		// You can also append an = to each operator to get it's
-		// assignment operator.
-
-	}
-
-	// Chapter 3.8a: Bit flags and masks
-	{
-		// Sometimes we want to use the invidivual bits in a
-		// data type. This is usually used for more memory-intensive
-		// tasks.
-		// Most time they are used for flags
-		unsigned char flags = 0b1101'0110;
-
-		// To get a specific bit, use bitwise AND
-		if (flags & 0b1000'0000) {
-			std::cout << "MSB is set." << std::endl;
-		}
-
-		// To turn a bit on, use bitwise OR
-		flags |= 0b0010'0000;
-
-		// To turn a bit off, use bitwise AND with an inverse bit pattern
-		flags &= ~0b0010'0000;
-
-		// To toggle a bit, use bitwise XOR
-		flags ^= 0b0010'0000;
-		flags ^= 0b0100'0000 | 0b1000'0000; // flips both
-
-		// std::bitset makes for easier use of bit flags and masks
-		std::bitset<8> bits(0b0000'0001); // Start with the LSB set
-
-		bits.set(4); // std::bitset uses indices. So this sets the 5th bit on
-		bits.flip(7); // MSB
-		bits.reset(7); // MSB
-
+		std::bitset<8> bits(0b0000'0001);
+		std::cout << bits << std::endl;
+		
+		bits.set(1); // uses indices
 		std::cout << bits << std::endl;
 
+		bits.flip(0);
+		std::cout << bits << std::endl;
+
+		bits.reset(1);
+		std::cout << bits << std::endl;
+
+		// Makes it easier to deal with bit masks and flags
 	}
 
-
-	// Chapter 4.1: Blocks (compound statements)
-	{
-		// A block (compound statement) is a group of statements that is 
-		// treated by the compiler as if it were a single statement.
-		// Blocks are started with a { and end with a }.
-	}
+	printSeparator();
 
 	// Chapter 4.1a: Local variables, scope, and duration
 	{
 		// scope is the area in which a variable is accessible
 		// duration determines where it is created and destroyed
 
-		// local variables are variables defined inside a block
 		// Local variables have automatic duration, which means they
 		// are created at the point of definition, and are destroyed
 		// when the block they are defined in is exited.
-		// Local variables have block scope (local scope), which means they
-		// enter scope at the point of declaration and go out of scope at the
-		// end of the block that they are defined in.
 
 		// Note: You can name a variable inside a nested block with the same
 		// name as a variable inside the outer block.
 		// This is called name hiding or shadowing.
 
-		// Rule: Avoid using nested variables with the same names
-		// as variables in an outer block.
-
-		// Rule: Defined variables in the smallest scope possible.
+		// Rule: Define variables in the smallest scope possible.
 		// By limiting the scope of a variable, you reduce the complexity
 		// of the program because the number of active variables is reduced.
 		// It also makes it easier to see where the variables are used.
-
 	}
+
+	printSeparator();
 
 	// Chapter 4.2: Global variables and linkage
 	{
-		// Variables declared outside of a block are called global variables.
 		// Global variables have static duration, which means they are created
 		// when the program starts and are destroyed when it ends.
 		// Global variables have global scope (also called global namespace scope,
-		// or file scope), which means they are visible until the end of the file 
-		// in which they are declared.
+		// or file scope)
 
 		// When a global variable is shadowed, but you want to access the global
 		// one, use the globa scope operator (::).
